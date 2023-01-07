a295bb5d82ebfdbb1e9de37e37a59e9b162697d5
Rioshanohan <rohanatl07@gmail.com>
2023-01-06 20:33:19 -0500

    all the stuffs

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-import cv2
-from cvzone.PoseModule import PoseDetector
+import cv2, os
+# from cvzone.PoseModule import PoseDetector
+imgs = []
+# for i in range(1,35):
+#     vid_name = "jab"+str(i)
 
-cap = cv2.VideoCapture('(636) How to Throw the Perfect Jab in Boxing - YouTube and 1 more page - Personal - Microsoft​ Edge 2022-11-22 13-44-09.mp4')
+#     cap = cv2.VideoCapture("./jab/"+vid_name+".mp4")
 
-detector = PoseDetector()
-posList = []
-while True:
-    try:
-        success, img = cap.read()
-        img = cv2.resize(img, (800,400))
-        img = detector.findPose(img)
-        lmList, bboxInfo = detector.findPosition(img)
+#     detector = PoseDetector()
+#     posList = []
+#     while True:
+#         try:
+#             success, img = cap.read()
+#             img = cv2.resize(img, (800,400))
+#             img = detector.findPose(img)
+#             lmList, bboxInfo = detector.findPosition(img)
 
-        if(bboxInfo):
-            lmString = ""
-            for lm in lmList:
-                lmString += f"{lm[1]},{img.shape[0]-lm[2]},{lm[3]};"
-            posList.append(lmString[:-1])
-        print(len(posList))
+#             if(bboxInfo):
+#                 lmString = ""
+#                 for lm in lmList:
+#                     lmString += f"{lm[1]},{img.shape[0]-lm[2]},{lm[3]};"
+#                 posList.append(lmString[:-1])
+#             print(len(posList))
 
-        cv2.imshow("Image",img)
-        key = cv2.waitKey(1)
-        if key == ord("s"):
-            break
+#             cv2.imshow("Image",img)
+#             key = cv2.waitKey(1)
+#             if key == ord("s"):
+#                 break
 
 
-    except:
-        print("no img left, ok please thank you very much love")
-        with open("BigBoyJab.txt", "w") as f:
-                f.writelines(["%s\n" % pos for pos in posList])
-        break
+#         except:
+#             print("no img left, ok please thank you very much love")
+#             with open("./jabt/"+vid_name, "w") as f:
+#                     f.writelines(["%s\n" % pos for pos in posList])
+            
+#             break
 
 # for f in frames:
 #     for i in range(0,33):
 #         bpy.data.collections["points"].objects["s"+str(i)].location = (locs[f][i][0], locs[f][i][1], locs[f][i][2])
 #         bpy.data.collections["points"].objects["s"+str(i)].keyframe_insert(data_path="location", frame=f)
+
+
+
+for vid in os.listdir("./jabt/"):
+    loc = []
+    with open("./jabt/"+vid) as f:
+        for line in f.readlines():
+            loc.append([i.split(",") for i in line.split(";")])
+    imgs.append(loc)
+print(imgs[0][0][0])
+
+# loc = []
+# with open("jab1") as f:
+#     for line in f.readlines():
+#         loc.append([i.split(",") for i in line.split(";")])
