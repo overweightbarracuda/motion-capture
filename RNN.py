1d168c5680d14847682ac50c0218c8ee3131c58c
overweightbarracuda <46425687+overweightbarracuda@users.noreply.github.com>
2023-01-08 21:09:12 -0500

    "There's something in my ass, mmmm mmmmm" - Nirvana

--- RNN.py
+++ RNN.py
@@ -10,63 +10,25 @@ from PIL import Image
 import math
 import os
 
-videos = []
-for vid in os.listdir("C://JTT/"):
-    loc = []
-    with open("C://JTT/"+vid) as f:
+video = []
+with open("C://JTT/jab4") as f:
         for line in f.readlines():
-            loc.append([i.split(",") for i in line.split(";")])
-    videos.append(loc)
+            video.append([i.split(",") for i in line.split(";")])
+            
+trainRNN = video[:-1]
+labelRNN = video[1:]
+for frame in labelRNN:
+    for coords in frame:
+        for coord in coords:
+            labelRNN[labelRNN.index(frame)][frame.index(coords)][coords.index(coord)] = int(labelRNN[labelRNN.index(frame)][frame.index(coords)][coords.index(coord)])
+for frame in trainRNN:
+    for coords in frame:
+        for coord in coords:
+            trainRNN[video.index(frame)][frame.index(coords)][coords.index(coord)] = int(trainRNN[video.index(frame)][frame.index(coords)][coords.index(coord)])
+trainRNN = torch.tensor(trainRNN, dtype=torch.float32)
+labelRNN = torch.tensor(labelRNN, dtype=torch.float32)
 
-trainRNN = []
-labelRNN = []
-for video in (videos):
-    trainRNN.append(videos[videos.index(video)][:-1])
-    labelRNN.append(videos[videos.index(video)][1:])
-for video in trainRNN:
-    for frame in video:
-        for coords in frame:
-            for coord in coords:
-                trainRNN[trainRNN.index(video)][video.index(frame)][frame.index(coords)][coords.index(coord)] = int(trainRNN[trainRNN.index(video)][video.index(frame)][frame.index(coords)][coords.index(coord)])
-for video in labelRNN:
-    for frame in video:
-        for coords in frame:
-            for coord in coords:
-                labelRNN[labelRNN.index(video)][video.index(frame)][frame.index(coords)][coords.index(coord)] = int(labelRNN[labelRNN.index(video)][video.index(frame)][frame.index(coords)][coords.index(coord)])
-
-trainRNN = torch.tensor(trainRNN)                
-labelRNN = torch.tensor(labelRNN)
-                
-                
-
-def trainerRNN(e, lgr,b):    
-    #criterion = nn.CrossEntropyLoss()
-    #criterion = my_loss()
-    criterion = nn.MSELoss()
-    #optimizer = optim.SGD(net.parameters(), lr=lgr, momentum=0.9)
-    optimizer = optim.Adam(RNNnet.parameters(), lr=lgr)
-    for epoch in range(e):  # iterations over the training data
-        running_loss = 0.0
-        #inputs = trainRNN.reshape(4,len(trainRNN),47,58)
-        #labels = labelRNN.reshape(4,len(labelRNN),47,58)
-        inputs = trainRNN
-        labels = labelRNN
-        # zero the parameter gradients
-        optimizer.zero_grad()
-        # change the weights
-        for i in range(int(len(inputs)/b)):
-            outputs, hidden = RNNnet(inputs[i:i+b])
-            outputs = torch.reshape(outputs,(b,12,47*58))
-            loss = criterion(outputs, labels[i:i+b])
-            loss.backward()
-            optimizer.step()
-                # print statistics
-            running_loss += loss.item()
-        print(epoch, running_loss)
-    print('Finished Training')
-    #print(torch.tensor(totouts).shape)
-    return outputs
-  class RNN(nn.Module):
+class RNN(nn.Module):
     def __init__(self, input_size, output_size, hidden_dim, n_layers):
         super(RNN, self).__init__()
         # Defining some parameters
@@ -78,6 +40,7 @@ def trainerRNN(e, lgr,b):
         self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
         # Fully connected layer
         self.fc = nn.Linear(hidden_dim, output_size)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
     def forward(self, x):
         
         batch_size = x.size(0)
@@ -90,7 +53,7 @@ def trainerRNN(e, lgr,b):
         
         # Reshaping the outputs such that it can be fit into the fully connected layer
         out = out.contiguous().view(-1, self.hidden_dim)
-        out = self.fc(out)
+        out = self.fc2(self.fc(out))
         
         return out, hidden
     
@@ -98,5 +61,30 @@ def trainerRNN(e, lgr,b):
         # This method generates the first hidden state of zeros which we'll use in the forward pass
         hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
          # We'll send the tensor holding the hidden state to the device we specified earlier as well
-        return hidden                 
+        return hidden
+
+
 RNNnet = RNN(input_size=33*3, output_size=33*3, hidden_dim=33*3, n_layers=1)
+
+def trainerRNN(e, lgr):
+    criterion = nn.MSELoss()
+    inputs, labels = trainRNN.reshape([42,33*3]), labelRNN.reshape([42,33*3])
+    inputs = torch.unsqueeze(inputs, dim=0)
+    labels = torch.unsqueeze(labels, dim=0)
+    print(inputs.shape, labels.shape)
+    optimizer = optim.Adam(RNNnet.parameters(), lr=lgr)
+    for i in range(e+1):
+        optimizer.zero_grad() # Clears existing gradients from previous epoch
+        output, hidden = RNNnet(inputs)
+        loss = criterion(output, inputs)
+        loss.backward() # Does backpropagation and calculates gradients
+        optimizer.step() # Updates the weights accordingly
+        if i%10 == 0:
+            print('Epoch: {}/{}.............'.format(i, e), end=' ')
+            print("Loss: {:.4f}".format(loss.item()))
+    return output, labels
+
+out = trainerRNN(1000, 5)
+
+for i in range(len(label[0])):
+    print(torch.squeeze(label, dim=0)[0][i]-out[0][i])
